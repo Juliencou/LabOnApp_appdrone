0cbf67435af54710d6be51f7242bac3b710bb838
Julien <julien.couyoupetrou@orange.fr>
2018-11-20 18:21:03 +0100

    3 activités pour commencer

--- wear/src/main/java/com/example/julien/appdrone/MainActivity.java
+++ wear/src/main/java/com/example/julien/appdrone/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.julien.appdrone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements View.OnClickListener {
 
     private TextView mTextView;
 
@@ -15,7 +18,29 @@ public class MainActivity extends WearableActivity {
 
         mTextView = (TextView) findViewById(R.id.text);
 
+        Button boutonfollow = findViewById(R.id.button);
+        boutonfollow.setOnClickListener(this);
+        Button boutoncontrol = findViewById(R.id.button2);
+        boutoncontrol.setOnClickListener(this);
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId()==R.id.button){
+            Intent intent=new Intent(MainActivity.this, activityfollow.class);
+            MainActivity.this.startActivity(intent);
+
+        }
+        else if (view.getId()==R.id.button2){
+            Intent intent2=new Intent(this, ActivityControl.class);
+            startActivity(intent2);
+
+
+        }
+
+
     }
 }
---
+++ wear/src/main/java/com/example/julien/appdrone/activityfollow.java
@@ -0,0 +1,14 @@
+package com.example.julien.appdrone;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityfollow extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityfollow);
+    }
+}
