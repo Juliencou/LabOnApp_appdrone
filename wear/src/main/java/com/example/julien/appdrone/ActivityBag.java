90a2d6aad08e226a45f11eac843476d4a821f902
azerty250 <36883459+azerty250@users.noreply.github.com>
2018-11-22 17:17:36 +0100

    Add list array

--- wear/src/main/java/com/example/julien/appdrone/ActivityBag.java
+++ wear/src/main/java/com/example/julien/appdrone/ActivityBag.java
@@ -1,4 +1,47 @@
 package com.example.julien.appdrone;
 
-public class ActivityBag {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ActivityBag extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bag);
+        final ListView item_list = (ListView) findViewById(R.id._item_bag_list);
+        final Button add_button = (Button) findViewById(R.id._add_bag_item_button);
+
+        // Initializing a new String Array
+        String[] items = new String[] {
+                "Keys",
+                "Phone"
+        };
+        final List<String> items_array = new ArrayList<String>(Arrays.asList(items));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, items_array);
+
+        item_list.setAdapter(arrayAdapter);
+
+        add_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+    }
 }
