49633183d8c5484bf27dd3fad070a76d09845896
azerty250 <36883459+azerty250@users.noreply.github.com>
2018-11-23 12:56:35 +0100

    Update

--- wear/src/main/java/com/example/julien/appdrone/ActivityBag.java
+++ wear/src/main/java/com/example/julien/appdrone/ActivityBag.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import android.widget.AdapterView.OnItemSelectedListener;
 
 
 public class ActivityBag extends Activity
